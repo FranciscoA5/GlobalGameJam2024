c863543b4f44e0ce0529104dc1c9d722bb0dcf53
MaciMani <joaomacieira14@gmail.com>
2024-01-27 19:46:52 +0000

    Bebado_actions(incomplete)

--- GlobalGameJam2024/Assets/Scripts/Woman.cs
+++ GlobalGameJam2024/Assets/Scripts/Woman.cs
@@ -74,23 +74,23 @@ public class Woman : Character
         }
     }
 
-    private void OnDrawGizmos()
-    {
-        Gizmos.color = Color.yellow;
-        Gizmos.DrawWireSphere(transf.position, screamRange);
-
-        // Draw a circle to visualize the attack range
-        float angleStep = 360f / circleSegments;
-        Vector3 prevPos = Vector3.zero;
-        for (int i = 0; i <= circleSegments; i++)
+        /*private void OnDrawGizmos()
         {
-            float angle = Mathf.Deg2Rad * i * angleStep;
-            Vector3 newPos = transf.position + new Vector3(Mathf.Cos(angle) * screamRange, Mathf.Sin(angle) * screamRange, 0f);
-            if (i > 0)
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawWireSphere(transf.position, screamRange);
+
+            // Draw a circle to visualize the attack range
+            float angleStep = 360f / circleSegments;
+            Vector3 prevPos = Vector3.zero;
+            for (int i = 0; i <= circleSegments; i++)
             {
-                Gizmos.DrawLine(prevPos, newPos);
+                float angle = Mathf.Deg2Rad * i * angleStep;
+                Vector3 newPos = transf.position + new Vector3(Mathf.Cos(angle) * screamRange, Mathf.Sin(angle) * screamRange, 0f);
+                if (i > 0)
+                {
+                    Gizmos.DrawLine(prevPos, newPos);
+                }
+                prevPos = newPos;
             }
-            prevPos = newPos;
-        }
-    }
+        }*/
 }
