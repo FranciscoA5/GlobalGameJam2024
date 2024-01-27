10df0f482104c5275b9fadd3b03f57170aa234e9
MaciMani <joaomacieira14@gmail.com>
2024-01-27 11:40:21 +0000

    FatManScript_plus_AnimationIdle

---
+++ GlobalGameJam2024/Assets/Scripts/Fat.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fat : Character
+{
+    public override void Idle()
+    {
+        Walk();
+    }
+
+    public override void Dead()
+    {
+       
+    }
+
+    public override void Active()
+    {
+
+    }
+}
