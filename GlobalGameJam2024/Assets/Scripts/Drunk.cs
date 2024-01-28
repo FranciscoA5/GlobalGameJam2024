ba18a2d423ac70595a4e615f6901a10f988e9265
MaciMani <joaomacieira14@gmail.com>
2024-01-28 14:14:07 +0000

    DrunkFinishing

--- GlobalGameJam2024/Assets/Scripts/Drunk.cs
+++ GlobalGameJam2024/Assets/Scripts/Drunk.cs
@@ -4,19 +4,11 @@ using UnityEngine;
 
 public class Drunk : Character
 {
-
-    private void OnMouseDown()
-    {
-        SwitchToIdleState(); // This line ensures that the previously first character goes back to idle state
-
-        // Change the state of the clicked woman to active
-        SwitchState(State.First);
-
-
-    }
+    private bool rage = false;
     public override void Active()
     {
         Walk();
+
     }
 
     public override void Dead()
@@ -27,10 +19,31 @@ public class Drunk : Character
     public override void Idle()
     {
 
+        if (Input.GetKey(KeyCode.E))
+        {
+            SwitchState(State.Reactive);
+        }
     }
 
     public override void Reactive()
     {
+        rage = true;
+    }
 
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (rage)
+        {
+            Vector2 forceDirection = new Vector2(30, 30);
+            collision.rigidbody.AddForce(forceDirection, ForceMode2D.Impulse);
+        }
+
+    }
+
+    public void Dance()
+    {
+        anim.SetTrigger("DrunkDance");
     }
 }
